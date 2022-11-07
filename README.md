c75e49ea36d625a0e0e1c5ffc529c00fcf486b18
Your Keeper <95983418+YourKeeper@users.noreply.github.com>
2022-11-06 22:03:07 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,18 @@
+# TUBESNAKE - THE SIMPLE PYTHON YOUTUBE DOWNLOADER
+
+![TubeSnake Logo](logo.png)
+
+<p>TubeSnake is a simple Python3 application for downloading YouTube videos in MP4 and MP3 format all nicely wrapped in a cross-platform Tk GUI. TubeSnake is lightweight in filesize and quite fast at downloading videos, and due to its small size and portable libraries can be run on all OSes capable of running the Tk graphical library. The application is simple for anyone to use and you know it can be trusted due to its open-source nature and simple-to-read coding language.</p>
+
+![TubeSnake](gui.png)
+
+## HOW TO USE
+
+<p>Setup is a breeze in TubeSnake thanks to the simple Setup Wizard script packaged with it, or Setup can be done within your Command Prompt or Terminal with either `pip install -r requirements.txt` on Unix-based OSes or `python -m pip install -r requirements.txt` in Windows. TubeSnake is very straight forward in its usage, first you take the URL of the YouTube video you wish to download and copy the Video ID from the URL.</p>
+
+***Example:***
+<p>https://www.youtube.com/watch?v=Gn-twrhKl-0 is the video we wish to download, so we take the string after the v= and copy-paste it into TubeSnake as such:</p>
+
+![Downloading a video in TubeSnake](demo.png)
+
+<p>TubeSnake will prompt for a directory to save the file into, and upon selection will proceed to download your video in either MP4 or MP3 file format to the chosen folder. Now you can sit back, relax, and enjoy your favourite YouTube videos without having to worry about using up your internet data.</p>
---
+++ tubesnake.py
@@ -0,0 +1,98 @@
+"""
+TUBESNAKE YOUTUBE DOWNLOADER - LICENSED UNDER THE BSD 2-CLAUSE LICENSE
+Copyright 2022 Karate Skid
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
+import pytube
+import tkinter
+import tkinter.filedialog
+
+class GUI:
+	def __init__(self):
+			# Creating Window object
+			self.window = tkinter.Tk()
+			self.window.geometry("350x320")
+			self.window.title("TubeSnake - YouTube Downloader")
+			self.icon = tkinter.PhotoImage(file = 'favicon.png')
+			self.window.iconphoto(False, self.icon)
+			self.canvas = tkinter.Canvas(self.window, width=200, height=200)
+
+			# Adding elements to Window
+			self.logo_img = tkinter.PhotoImage(file = 'logo.png')
+			self.TSlabel = tkinter.Label(self.window, text="Enter the Video ID")
+			self.entry = tkinter.Entry(self.window, fg="black", bg="white", width=25)
+			self.status_label = tkinter.Label(self.window, text=" ")
+
+			# Buttons and shit
+			self.frame = tkinter.Frame(self.window)
+			self.frame.columnconfigure(0, weight=1)
+			self.frame.columnconfigure(1, weight=1)
+			self.frame.columnconfigure(2, weight=1)
+			self.download_mp4_button = tkinter.Button(self.frame, text="Download MP4", width=10, height=1, bg="white", fg="black", command=self.download_mp4)
+			self.download_mp3_button = tkinter.Button(self.frame, text="Download MP3", width=10, height=1, bg="white", fg="black", command=self.download_mp3)
+			self.close_button = tkinter.Button(self.frame, text="Exit", width=10, height=1, bg="white", fg="black", command=self.window.destroy)
+
+			# Pack it up
+			self.canvas.create_image(100, 100, image=self.logo_img)
+			self.canvas.pack()
+			self.TSlabel.pack()
+			self.entry.pack(padx=5, pady=5)
+			self.status_label.pack()
+			self.download_mp4_button.grid(row=0, column=0, sticky=tkinter.W+tkinter.E)
+			self.download_mp3_button.grid(row=0, column=1, sticky=tkinter.W+tkinter.E)
+			self.close_button.grid(row=0, column=2, sticky=tkinter.W+tkinter.E)
+			self.frame.pack()
+			self.window.mainloop()
+
+	def throw_error(self, error):
+		error_msg = f"""
+		Errors have occured! Please examine the following Error output: {error}
+		Please re-run TubeSnake with properly provided arguments, if you encounter this Error again, please report it on our GitHub @ github.com
+		"""
+		self.status_label.configure(text="Error!")
+		tkinter.messagebox.showerror(title="An Error Occured!", message=error_msg)
+		self.status_label.configure(text=" ")
+
+	def download_mp4(self):
+		video_ID = self.entry.get()
+		link = f"https://www.youtube.com/watch?v={video_ID}"
+		self.status_label.configure(text="Downloading, please wait...")
+		save_loc = tkinter.filedialog.askdirectory()
+
+		try:
+			video = pytube.YouTube(link)
+			video.streams.filter(file_extension="mp4")
+			video.streams.get_highest_resolution().download(output_path=save_loc)
+			self.status_label.configure(text=" ")
+			tkinter.messagebox.showwarning(title="Download Completed!", message=f"Video downloaded and saved to {save_loc}!")
+		except Exception as error:
+			self.throw_error(error)
+
+	def download_mp3(self):
+		video_ID = self.entry.get()
+		link = f"https://www.youtube.com/watch?v={video_ID}"
+		self.status_label.configure(text="Downloading, please wait...")
+		save_loc = tkinter.filedialog.askdirectory()
+
+		try:
+			video = pytube.YouTube(link)
+			video.streams.filter(only_audio=True)
+			video.streams.get_highest_resolution().download(output_path=save_loc)
+			self.status_label.configure(text=" ")
+			tkinter.messagebox.showwarning(title="Download Completed!", message=f"Video downloaded and saved to {save_loc}!")
+		except Exception as error:
+			self.throw_error(error)
+
+def main():
+	app = GUI()
+
+if __name__ == "__main__":
+	main()
